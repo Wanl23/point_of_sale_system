c160a27c2700ce560b9a911cad6cc4f6c094b331
IvanLeschinsky <ivan.leschinsky@yandex.ru>
2019-07-08 00:15:24 +0500

    add test TotalPriceServiceTest plus method - check that total qty increase when there are Special offer

--- src/main/java/app/services/TotalPriceService.java
+++ src/main/java/app/services/TotalPriceService.java
@@ -2,24 +2,37 @@ package app.services;
 
 import app.Entityes.Product;
 import app.Entityes.Sale;
+import app.Entityes.SpecialOffer;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TotalPriceService {
 
     private int totalPrice;
+    private Map<Product, Integer> totalQty;
     private List<Sale> saleList;
+    private List<SpecialOffer> specialOfferList;
 
-    TotalPriceService(List<Sale> saleList) {
+    TotalPriceService(List<Sale> saleList, List<SpecialOffer> specialOfferList) {
         this.saleList = saleList;
+        this.specialOfferList = specialOfferList;
     }
 
-    public int plus(Product product) {
+    public int plus(Product product, int qty) {
+        if (totalQty == null) totalQty = new HashMap<>();
         saleList.stream().filter(s -> s.getProduct() == product)
                 .forEach(s -> {
                     product.setPrice(s.getNewPrice());
                 });
-        totalPrice = totalPrice + product.getPrice();
+        specialOfferList.stream().filter(s -> s.getProduct() == product)
+                .forEach(s -> {
+                    if (s.getQtyNeedToBuy() == qty) {
+                        totalQty.put(product, qty + 1);
+                    }
+                });
+        totalPrice = totalPrice + product.getPrice() * qty;
         return totalPrice;
     }
 
@@ -27,4 +40,7 @@ public class TotalPriceService {
         return totalPrice;
     }
 
+    public int getTotalQty(Product product) {
+        return totalQty.get(product);
+    }
 }
--- src/test/java/app/services/TotalPriceServiceTest.java
+++ src/test/java/app/services/TotalPriceServiceTest.java
@@ -2,10 +2,10 @@ package app.services;
 
 import app.Entityes.Product;
 import app.Entityes.Sale;
+import app.Entityes.SpecialOffer;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +15,8 @@ public class TotalPriceServiceTest {
     Product product;
     List<Sale> saleList;
     Sale sale;
+    SpecialOffer specialOffer;
+    List<SpecialOffer> specialOfferList;
 
     @Before
     public void setUp() throws Exception {
@@ -22,24 +24,35 @@ public class TotalPriceServiceTest {
         sale = new Sale(product, 4);
         saleList = new ArrayList<>();
         saleList.add(sale);
-        totalPriceService = new TotalPriceService(saleList);
-    }
 
+        specialOffer = new SpecialOffer(product, 2, 1);
+        specialOfferList = new ArrayList<>();
+        specialOfferList.add(specialOffer);
+
+        totalPriceService = new TotalPriceService(saleList, specialOfferList);
+    }
 
     @Test
     public void WhenAddNewProductForCalculatingItReturnIncreasedFullSum() {
-        Assert.assertEquals(totalPriceService.plus(product), product.getPrice());
+        Assert.assertEquals(totalPriceService.plus(product, 1), product.getPrice());
     }
 
     @Test
     public void WhenAddNewProductForCalculatingItIncreasedTotalPrice() {
-        totalPriceService.plus(product);
+        totalPriceService.plus(product, 1);
         Assert.assertEquals(totalPriceService.getTotalPrice(), product.getPrice());
     }
 
     @Test
     public void WhenAddProductForCalcAndThereAreSaleForThisProductItUseSumFromSale() {
-        totalPriceService.plus(product);
+        totalPriceService.plus(product, 1);
         Assert.assertEquals(totalPriceService.getTotalPrice(), sale.getNewPrice());
+    }
+
+    @Test
+    public void WhenAddProductForCalcAndThereAreSpecialOfferItAddFreeProductIfOrderedQtyMatchOffer() {
+        int qty = 2;
+        totalPriceService.plus(product, qty);
+        Assert.assertEquals(totalPriceService.getTotalQty(product), qty + specialOffer.getGetFree());
     }
 }

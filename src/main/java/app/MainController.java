b51d8057816ad28258426c5333fa1284e744756a
IvanLeschinsky <ivan.leschinsky@yandex.ru>
2019-07-07 17:47:35 +0500

    Initial commit

---
+++ src/main/java/app/MainController.java
@@ -0,0 +1,9 @@
+package app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("")
+public class MainController {
+}
